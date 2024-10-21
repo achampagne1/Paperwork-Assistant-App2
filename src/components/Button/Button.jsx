78451eaec5ea186a4f366315f0206aef378472fd
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2024-10-20 22:11:30 -0500

    opacity changed now

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { File1 } from "../../icons/File1";
 import "./style.css";
+import "../../screens/IphoneProMax/style.css"
 
 export const Button = ({ className }) => {
     const [selectedFile, setSelectedFile] = useState(null);
@@ -11,19 +12,23 @@ export const Button = ({ className }) => {
         console.log("Selected file:", file);
     };
 
+    function handleClick() {
+        // Change the opacity of the button
+        const button = document.querySelector('.button-3');
+        if (button) { 
+            button.style.opacity = '1.0';
+        }
+
+        // Open the file system dialog
+        const fileInput = document.getElementById('fileInput');
+        fileInput.click(); 
+    }
+
   return (
     <div className={`button ${className}`}>
-          <input
-              type="file"
-              id="fileInput"
-              style={{ display: "none" }}
-              onChange={handleFileChange}
-          />
-          <div
-              className="button"
-              onClick={() => document.getElementById("fileInput").click()}  // Trigger file input
-          >
-      <File1 className="file" color="#F5F5F5" />
+          <input type="file" id="fileInput" style={{ display: "none" }} onChange={handleFileChange}/>
+          <div className="button" onClick={() => handleClick()}>
+              <File1 className="file" color="#F5F5F5" />
               <button className="text-wrapper">Select File</button>
           </div>
           {selectedFile && <p className="file-name">Selected File: {selectedFile.name}</p>}
